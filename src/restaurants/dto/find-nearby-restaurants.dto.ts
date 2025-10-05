4dc58ee0ca833c6ffa46f7eebfc12fc5fc3e74d2
AKhaled22 <ahmedkhaled1029@gmail.com>
2025-10-05 16:47:18 +0300

    remove unwanted checks

--- src/restaurants/dto/find-nearby-restaurants.dto.ts
+++ src/restaurants/dto/find-nearby-restaurants.dto.ts
@@ -1,20 +1,16 @@
 import { IsNumber, IsOptional, Min, Max } from 'class-validator';
-import { Type } from 'class-transformer';
 
 export class FindNearbyRestaurantsDto {
-  @Type(() => Number)
   @IsNumber()
   @Min(-180)
   @Max(180)
   longitude: number;
 
-  @Type(() => Number)
   @IsNumber()
   @Min(-90)
   @Max(90)
   latitude: number;
 
-  @Type(() => Number)
   @IsOptional()
   @IsNumber()
   @Min(0.1)
